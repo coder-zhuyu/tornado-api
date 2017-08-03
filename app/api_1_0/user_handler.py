0cfa1ec997832244a96c553c4778344735bd5224
rain <zhuyu@think-land.com>
2017-08-03 16:51:22 +0800

    add schema for validating data

--- app/api_1_0/user_handler.py
+++ app/api_1_0/user_handler.py
@@ -2,6 +2,7 @@
 from ..db import Db
 from ..request_handler import BaseRequestHandler
 from ..log import debug_log, info_log, warning_log, error_log
+from schema import Schema, Regex
 
 
 class UserHandler(BaseRequestHandler):
@@ -11,6 +12,12 @@ class UserHandler(BaseRequestHandler):
         self.response_json(result=result)
 
     async def put(self, user_id):
+        phone = self.get_argument('phone')
+        name = self.get_argument('name')
+
+        Regex(r'^1[0-9]{10}$').validate(phone)
+        Schema(str).validate(name)
+
         affected_rows = await Db.update("UPDATE user_admin set phone='11111111111' where id=%s", int(user_id))
         debug_log(affected_rows)
         self.response_json(result={'affected_rows': affected_rows})
