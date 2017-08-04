b4f6f1bf5c1fc2026c6013c4fd5385bfd6eb5fcc
rain <zhuyu@think-land.com>
2017-08-04 14:52:22 +0800

    add settings

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
 import tornado.web
-from .api_1_0 import UserHandler
+from config import config
+from .api_1_0 import UserHandler, AuthLoginHandler, AuthLogoutHandler
 
 
 def create_app():
-    app = tornado.web.Application([
+    settings = dict(
+        cookie_secret=config.cookie_secret,
+        debug=config.debug
+    )
+
+    handlers = [
         ("/user/([0-9]+)", UserHandler),
-    ])
+        ("/auth/login", AuthLoginHandler),
+        ("/auth/logout", AuthLogoutHandler),
+    ]
+
+    app = tornado.web.Application(handlers, **settings)
 
     return app
