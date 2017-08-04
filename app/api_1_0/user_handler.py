d81f34e839808977423e8039830832d4b0aaf3c0
rain <zhuyu@think-land.com>
2017-08-04 14:51:22 +0800

    user get, update

--- app/api_1_0/user_handler.py
+++ app/api_1_0/user_handler.py
@@ -1,16 +1,22 @@
 # -*- coding: utf-8 -*-
 from ..db import Db
-from ..request_handler import BaseRequestHandler
+from ..request_handler import BaseRequestHandler, login_required
 from ..log import debug_log, info_log, warning_log, error_log
 from schema import Schema, Regex
 
 
 class UserHandler(BaseRequestHandler):
+    @login_required
     async def get(self, user_id):
-        result = await Db.select_one("SELECT id, username, phone, email FROM user_admin where id=%s limit 1", int(user_id))
+        result = await Db.select_one("SELECT id, username, phone, email FROM user_admin where id=%s limit 1",
+                                     int(user_id))
         debug_log(result)
-        self.response_json(result=result)
+        if result is None:
+            self.response_json(code='600100')
+        else:
+            self.response_json(result=result)
 
+    @login_required
     async def put(self, user_id):
         phone = self.get_argument('phone')
         name = self.get_argument('name')
@@ -18,6 +24,10 @@ class UserHandler(BaseRequestHandler):
         Regex(r'^1[0-9]{10}$').validate(phone)
         Schema(str).validate(name)
 
-        affected_rows = await Db.update("UPDATE user_admin set phone='11111111111' where id=%s", int(user_id))
+        affected_rows = await Db.update("UPDATE user_admin set phone=%s, name=%s where id=%s",
+                                        (phone, name, int(user_id)))
         debug_log(affected_rows)
-        self.response_json(result={'affected_rows': affected_rows})
+        if affected_rows is -1:
+            self.response_json(code='600101')
+        else:
+            self.response_json()
